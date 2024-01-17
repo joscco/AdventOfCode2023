90cb2954f48c2f5ae044a507b2bd4f5c43cedfa8
Jonathan Schmitz <jomitz96@hotmail.com>
2024-01-17 22:51:47 +0100

    Improve solution for day 25

--- src/days/25/Solution.ts
+++ src/days/25/Solution.ts
@@ -1,10 +1,9 @@
 import {AbstractSolution} from "../../types/AbstractSolution";
 import {Str} from "../../types/Str";
 import {Set} from "../../types/Set";
-import {Arr} from "../../types/Arr";
+import {Dict} from "../../types/Dict";
 
 type Node = string
-type Edge = [Node, Node]
 
 export class Solution extends AbstractSolution {
 
@@ -18,7 +17,7 @@ export class Solution extends AbstractSolution {
 
     solveFirst(input: Str, test: boolean): string {
         let nodes = new Set<Node>(node => node)
-        let edges = new Set<Edge>(edge => edge[0] + "_" + edge[1])
+        let neighborMap = new Dict<Node, Node[]>(node => node)
 
         for (let row of input.parseRows().map(row => row.toString().replace(":", "").split(" ")).toArray()) {
             let source = row[0]
@@ -26,17 +25,24 @@ export class Solution extends AbstractSolution {
             for (let i = 1; i < row.length; i++) {
                 let aim = row[i]
                 nodes.add(aim)
-                edges.add([source, aim])
+                let sourceNeighbors = neighborMap.get(source) ?? []
+                sourceNeighbors.push(aim)
+                let aimNeighbors = neighborMap.get(aim) ?? []
+                aimNeighbors.push(source)
+
+                neighborMap.set(source, sourceNeighbors)
+                neighborMap.set(aim, aimNeighbors)
             }
         }
 
         let S = nodes.copy()
         let S_C = new Set<Node>(n => n)
-        while (new Arr(S.values().map(s => this.countNeighborsIn(s, S_C, edges))).toNumArr().sum() != 3) {
-            let maxNeighborNode = S.values().map(s => [s, this.countNeighborsIn(s, S_C, edges)] as [Node, number]).sort(([x, u], [y, v]) => v - u)[0][0]
+        while (S.values().map(s => this.countNeighborsIn(s, S_C, neighborMap)).reduce((a, b) => a + b) != 3) {
+            let maxNeighborNode = S.values()
+                .map(s => [s, this.countNeighborsIn(s, S_C, neighborMap)] as [Node, number])
+                .sort(([, u], [, v]) => v - u)[0][0]
             S.remove(maxNeighborNode)
             S_C.add(maxNeighborNode)
-            console.log(S_C.values().length)
         }
 
         return (S.values().length * S_C.values().length).toString()
@@ -46,7 +52,7 @@ export class Solution extends AbstractSolution {
         return "NONE"
     }
 
-    private countNeighborsIn(s: Node, G: Set<Node>, edges: Set<Edge>): number {
-        return G.values().filter(g => edges.has([s, g]) || edges.has([g, s])).length
+    private countNeighborsIn(s: Node, G: Set<Node>, neighborMap: Dict<Node, Node[]>): number {
+        return neighborMap.get(s)!.filter(g => G.has(g)).length
     }
 }
